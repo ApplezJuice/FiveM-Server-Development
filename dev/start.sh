64cb302fe04a6c2643c98cdc2ab68b6c0ff4892b
DevEnv <mghayes93@gmail.com>
2019-05-15 15:44:51 -0700

    Added in initial structure

---
+++ dev/start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /home/svcfivem/FiveM-Server-Development/dev/server-data
+
+tmux new-session -d -s gtaserver '/home/svcfivem/FiveM-Server-Development/dev/server/run.sh +exec server.cfg'
