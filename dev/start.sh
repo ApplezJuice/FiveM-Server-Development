b9037873f6d11cb57bb1b7a5b30b301aea1c12fa
DevEnv <mghayes93@gmail.com>
2019-05-15 15:47:50 -0700

    Remove gitignore to reupload

--- dev/start.sh
+++ dev/start.sh
@@ -2,4 +2,4 @@
 
 cd /home/svcfivem/FiveM-Server-Development/dev/server-data
 
-tmux new-session -d -s gtaserver '/home/svcfivem/FiveM-Server-Development/dev/server/run.sh +exec server.cfg'
+tmux new-session -d -s gtaserver '/home/svcfivem/FiveM-Server-Development/dev//server/run.sh +exec server.cfg'
